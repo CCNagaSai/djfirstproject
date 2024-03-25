0d0e253bc6f1c76f7694e4f037f35e55e3f8ad68
Naga_Sai <ccnagasai2@gmail.com>
2024-03-25 15:20:55 +0530

    In this branch, learn and praticed templates and views how to write from w3schools

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2cb*oi%_2%anugc%ft69u6j2(s2paje_+*a+d612w7!x^fv+4('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
